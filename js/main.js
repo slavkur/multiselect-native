f6e2ac01ad5693cf4ad8634fe2c8ddf15a93f4f5
Slava Kurkov <slavkur2013@gmail.com>
2015-05-05 18:54:23 +0300

    further updates

--- js/main.js
+++ js/main.js
@@ -1,43 +1,6 @@
-/*
-
-{
-A:'Ф', // 0041 0424
-B:'И' // 0042 0418
-C:
-D:
-E:
-F:
-G:
-H:
-K:
-L:
-M:
-N:
-O:
-P:
-'[' // 005B '{' 007B
-']' // 005D '}' 007D
-':', 003A '; '003B
-'<' ','
-'<'" '.'
-} 
-
-
-*/
-
-var qwrus = 'ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ';
-var qwrusMin = qwrus.toLowerCase();
-var qweng = 'QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>';
-var qwengMin = qweng.toLowerCase();
-var codes = [81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 90, 88, 67, 86, 66, 78, 77, 188, 190];
 var keyDown = 40;
 var keyUp = 38;
 var keyEnter = 13;
-
-function multyKey(keyCode) {
-  var codeIndex = codes.indexOf(e.keyCode);
-  return (e.keyCode, qwrus[codeIndex], qweng[codeIndex]);
-}
 
 function xmlhttpreq() {
   var xmlhttp;
@@ -55,119 +18,184 @@ function xmlhttpreq() {
   }
   return xmlhttp;
 }
-var http = xmlhttpreq();
-http.open('GET', '/', false);
-http.send(null);
-if (http.status == 200) {
-  console.log(http.status);
+
+function request(url, callback) {
+  var http = xmlhttpreq();
+  http.open('GET', url, false);
+  http.onload = function() {
+    if (http.status === 200) {
+      callback(http, JSON.parse(http.response));
+    }
+  };
+  http.send(null);
 }
 
-var list = ["Vladimir Evseenkov", "Sofya Kurkova", "Lisa Bobrykava", "Sergey Scherbakov",
-  "Stanislav Zinovyev", "Arturo Latypov", "Andrey Zhadanov", "Maria Prikhodko", "Kristina Kolyakina",
-  "Timur Novikov", "Alex Frants", "Vladislav Guskov", "Artur Bekirov", "Mikhail Yatsyk", "Maxim Usik",
-  "Zoychik Bogach", "Svetlana Gusterina", "Masha Petrova"
-];
-var filtered;
+function renderSelectors(cls) {
+  var selectors = document.getElementsByClassName(cls);
+  for (var i = 0; i < selectors.length; i++) {
+    Selector(selectors[i]);
+  }
+}
 
-// Selector
-var selector = document.getElementsByClassName('selector')[0];
-var filter = selector.firstChild;
-var dialog = selector.nextSibling.firstChild;
-document.body.onclick = function(e) {
-  selector.nextSibling.style.display = null;
-};
-selector.onclick = function(e) {
-  e.stopPropagation();
-  filter.focus();
-};
-dialog.onclick = function(e) {
-  e.stopPropagation();
-};
-filter.onclick = function(e) {
-  e.stopPropagation();
-};
-filter.onfocus = function(e) {
-  selector.nextSibling.style.display = 'block';
-};
-filter.onkeydown = function(e) {
-  //console.log(e.keyCode, selector.nextSibling.clientHeight, selector.nextSibling.scrollTop, selected.offsetTop);
-  if (keyDown === e.keyCode && selected.nextSibling) {
-    selected.removeAttribute('class');
-    selected.nextSibling.setAttribute('class', 'selected');
-    selected = selected.nextSibling;
-    if (selector.nextSibling.clientHeight + selector.nextSibling.scrollTop - selected.clientHeight < selected.offsetTop) {
-      selector.nextSibling.scrollTop += selected.clientHeight;
-    }
-  } else if (keyUp === e.keyCode && selected.previousSibling) {
-    selected.removeAttribute('class');
-    selected.previousSibling.setAttribute('class', 'selected');
-    selected = selected.previousSibling;
-    if (selector.nextSibling.scrollTop > selected.offsetTop) {
-      selector.nextSibling.scrollTop -= selected.clientHeight;
-    }
-  } else if (keyEnter === e.keyCode) {
-    var person = document.createElement('div');
-    var label = document.createElement('label');
-    var del = document.createElement('i');
-    del.innerText = "x";
-    del.onclick = function(e) {
-      e.stopPropagation();
-      selector.removeChild(person);
+function Selector(selector) {
+  var filtered;
+  var dialogHeight;
+  var selectedPerson;
+  var added = [];
+  var filter = selector.firstChild;
+  var dialog = selector.nextSibling;
+  var dialogList = dialog.firstChild;
+  var bodyBlur = function(e) {
+    dialog.style.display = null;
+  };
+  // multiinstance events
+  if(document.body.onclick instanceof Function) {
+    var bodyClick = document.body.onclick;
+    document.body.onclick = function() {
+      bodyClick();
+      bodyBlur();
+    };
+  } else {
+    document.body.onclick = bodyBlur;
+  }
+
+  selector.onclick = function(e) {
+    // stop bubbling to keep dialog open
+    e.stopPropagation();
+    filter.focus();
+  };
+  dialogList.onclick = function(e) {
+    // stop bubbling to keep dialog open
+    e.stopPropagation();
+  };
+  filter.onclick = function(e) {
+    // stop bubbling to keep dialog open
+    e.stopPropagation();
+  };
+  filter.onfocus = function(e) {
+    // clear all opened dialogs by bluring
+    document.body.onclick();
+    dialog.style.display = 'block';
+    dialogHeight = dialog.clientHeight;
+  };
+  filter.onkeydown = function(e) {
+    // key navigation support, arrow up/down with proper scrolling
+    if (keyDown === e.keyCode && selectedPerson.nextSibling) {
+      selectedPerson.removeAttribute('class');
+      selectedPerson.nextSibling.setAttribute('class', 'selected');
+      selectedPerson = selectedPerson.nextSibling;
+      if (dialog.clientHeight + dialog.scrollTop - selectedPerson.clientHeight < selectedPerson.offsetTop) {
+        dialog.scrollTop += selectedPerson.clientHeight;
+      }
+    } else if (keyUp === e.keyCode && selectedPerson.previousSibling) {
+      selectedPerson.removeAttribute('class');
+      selectedPerson.previousSibling.setAttribute('class', 'selected');
+      selectedPerson = selectedPerson.previousSibling;
+      if (dialog.scrollTop > selectedPerson.offsetTop) {
+        dialog.scrollTop -= selectedPerson.clientHeight;
+      }
+    } else if (keyEnter === e.keyCode) {
+      var person = document.createElement('div');
+      var label = document.createElement('label');
+      var del = document.createElement('i');
+
+      del.innerText = "x";
+      del.onclick = function(e) {
+        // stop bubbling to prevent focusing on selector
+        e.stopPropagation();
+        selector.removeChild(person);
+        added.splice(added.indexOf(e.target.previousSibling.innerText), 1);
+
+        document.body.onclick();
+        filter.blur();
+      };
+      person.appendChild(label);
+      person.appendChild(del);
+      selector.insertBefore(person, filter);
+      label.innerText = selectedPerson.innerText;
+      added.push(selectedPerson.innerText);
+
       document.body.onclick();
       filter.blur();
-    };
-    person.appendChild(label);
-    person.appendChild(del);
-    selector.insertBefore(person, filter);
-    label.innerText = selected.innerText;
-    document.body.onclick();
-    filter.blur();
-  }
-};
-filter.onkeyup = function(e) {
-  if (e.keyCode !== keyDown && e.keyCode !== keyUp) {
-    filtered = [];
-    for (var i = 0; i < list.length; i++) {
-      if (list[i].toLowerCase().indexOf(e.target.value.toLowerCase()) > -1) {
-        filtered.push(list[i]);
+      filter.value = '';
+      People(list);
+    }
+  };
+  filter.onkeyup = function(e) {
+    // text filtering if no arrows for navigation used
+    if (e.keyCode !== keyDown && e.keyCode !== keyUp) {
+      filtered = [];
+      // first we check local data received with initial request
+      for (var i = 0; i < list.length; i++) {
+        if (list[i].toLowerCase().indexOf(e.target.value.toLowerCase()) > -1) {
+          filtered.push(list[i]);
+        }
+      }
+      if (e.target.value.length === 0) {
+        People(list);
+      } else if(filtered.length === 0) {
+        request('/api/people/'+ e.target.value, function(http, jsonResp) {
+          People(jsonResp);
+        });
+      } else {
+        People(filtered);
       }
     }
-    if (e.target.value.length === 0) {
-      People(list);
+  }
+
+  function People(list) {
+    var cummulativeHeight = 0;
+    dialogList.innerHTML = '';
+    for (var i = 0; i < list.length; i++) {
+      if(added.indexOf(list[i]) === -1) {
+        var person = document.createElement('li');
+        var avatar = document.createElement('i');
+        // self execution function to ensure the right 'person' is used
+        (function(person) {
+          person.onmouseover = function() {
+            selectedPerson.removeAttribute('class');
+            person.setAttribute('class', 'selected');
+            selectedPerson = person;
+          };
+          person.onclick = function() {
+            filter.onkeydown({
+              keyCode: keyEnter
+            });
+          };
+        })(person);
+        person.innerText = list[i];
+        dialogList.appendChild(person);
+        person.insertBefore(avatar, person.firstChild);
+        cummulativeHeight += person.clientHeight;
+        if (i === 0) {
+          selectedPerson = person;
+          person.setAttribute('class', 'selected');
+        }
+      }
+    }
+    adjustDialogHeight(cummulativeHeight, list.length > 0);
+  }
+
+  function adjustDialogHeight(cummulativeHeight, hasItems) {
+    console.log(cummulativeHeight, dialog.clientHeight);
+    if(cummulativeHeight <= dialogHeight && hasItems && cummulativeHeight > 0) {
+      dialog.style.height = cummulativeHeight;
     } else {
-      People(filtered);
+      dialog.style.height = null;
+    }
+    // to avoid interaption wuth selector>dialog
+    if(!hasItems) {
+      dialog.style.visibility = 'hidden';
+    } else {
+      dialog.style.visibility = null;
     }
   }
+
+  People(list);
 }
 
-// Person
-var selected;
-
-function People(list) {
-  dialog.innerHTML = '';
-  for (var i = 0; i < list.length; i++) {
-    var person = document.createElement('li');
-    var avatar = document.createElement('i');
-    // self execution function to ensure the right 'person' is used
-    (function(person) {
-      person.onmouseover = function() {
-        selected.removeAttribute('class');
-        person.setAttribute('class', 'selected');
-        selected = person;
-      };
-      person.onclick = function() {
-        filter.onkeydown({
-          keyCode: keyEnter
-        });
-      };
-    })(person);
-    person.innerText = list[i];
-    dialog.appendChild(person);
-    person.insertBefore(avatar, person.firstChild);
-    if (i === 0) {
-      selected = person;
-      person.setAttribute('class', 'selected');
-    }
-  }
-}
-People(list);
+var list = [];
+request('/api/people', function(http, jsonResp) {
+  list = jsonResp;
+  renderSelectors('selector');
+});
--- server.js
+++ server.js
@@ -6,7 +6,119 @@ server.use('/js', express.static(__dirname + '/js'));
 server.use('/styles', express.static(__dirname + '/styles'));
 
 server.get('/', function(req, res) {
-    res.render(__dirname + '/index.html');
+  res.render(__dirname + '/index.html');
+});
+
+server.get('/api/people', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(listAll));
+});
+server.get('/api/people/:query', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(filterPeople(req.params.query)));
 });
 
 server.listen(8080, '127.0.0.1');
+
+
+// keys must match with keyboard layout for transcoding
+var qwrus = 'ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ';
+var qwrusMin = qwrus.toLowerCase();
+var qweng = 'QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>';
+var qwengMin = qweng.toLowerCase()
+.replace('{', '[').replace('}', ']')
+.replace(':', ';').replace('"', "'")
+.replace('<', ',').replace('>', '.');
+
+function findChar(origLetter, lang1, lang2, lang1Min, lang2Min) {
+  // both small and uppercases are checked (due to the properties of keyboard layout)
+  var letter;
+  letter = lang1[lang2.indexOf(origLetter)];
+  if(!letter) {
+    letter = lang1Min[lang2Min.indexOf(origLetter)];
+  }
+  if(!letter) {
+    // original letter will be used if no transcoding done
+    letter = origLetter;
+  }
+  return letter;
+}
+
+function transcode(str) {
+  var result = '';
+  for(var i = 0; i < str.length; i++) {
+    // hard coded distinction experession and optimization
+    // english char codes are less than 1000
+    // russian char codes are larger than 1000
+    if(str[i].charCodeAt(0) < 1000) {
+      result += findChar(str[i], qwrus, qweng, qwrusMin, qwengMin);
+    } else {
+      result += findChar(str[i], qweng, qwrus, qwengMin, qwrusMin);
+    }
+  }
+  return result;
+}
+
+function filterPeople(query) {
+  // we must check both name and domain for proper search (+in case of transcoding)
+  var filtered = [];
+  people.forEach(function(item) {
+    if(item.name.toLowerCase().indexOf(query) > -1 || 
+       item.name.toLowerCase().indexOf(transcode(query)) > -1) {
+      filtered.push(item.name);
+    } else if(item.domain && 
+      (item.domain.toLowerCase().indexOf(query) > -1  ||
+       item.domain.toLowerCase().indexOf(transcode(query)) > -1)) {
+      filtered.push(item.name);
+    }
+  });
+  return filtered;
+}
+
+var people = [{
+    name: "Vladimir Evseenkov",
+    domain: "hellovlad"
+  }, {
+    name: "Sofya Kurkova",
+    domain: "sofa"
+  }, {
+    name: "Lisa Bobrykava"
+  }, {
+    name: "Sergey Scherbakov"
+  }, {
+    name: "Stanislav Zinovyev"
+  }, {
+    name: "Arturo Latypov"
+  }, {
+    name: "Andrey Zhadanov"
+  }, {
+    name: "Maria Prikhodko"
+  }, {
+    name: "Kristina Kolyakina"
+  }, {
+    name: "Timur Novikov"
+  }, {
+    name: "Alex Frants"
+  }, {
+    name: "Vladislav Guskov",
+    domain: 'rus123'
+  }, {
+    name: "Artur Bekirov"
+  }, {
+    name: "Mikhail Yatsyk"
+  }, {
+    name: "Maxim Usik"
+  }, {
+    name: "Zoychik Bogach"
+  }, {
+    name: "Svetlana Gusterina"
+  }, {
+    name: "Masha Petrova"
+  } 
+];
+
+// list for all people needed as the start
+var listAll = [];
+people.forEach(function(item) {
+  listAll.push(item.name);
+});
